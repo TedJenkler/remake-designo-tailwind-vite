8eebc9639bc2ca61e0a8365e2aedabacd6f7b124
TedJenkler <Tedjenkler@gmail.com>
2024-03-05 09:53:33 +0100

    locations tablet done

--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -4,9 +4,9 @@ import 'leaflet/dist/leaflet.css';
 
 function Locations() {
   return (
-    <main className='mb-80'>
-      <div className='flex flex-col items-center h-screen mb-10'>
-        <div className='h-80 w-full'>
+    <main className='mb-80 md:mb-96'>
+      <div className='flex flex-col items-center h-screen mb-10 md:mx-10 md:mb-32'>
+        <div className='h-80 w-full md:rounded-2xl md:mb-8 overflow-hidden'>
           <MapContainer center={["43.648233", "-79.375602"]} zoom={13} style={{ height: '100%', width: '100%', margin: 0, padding: 0 }} zoomControl={false}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -19,7 +19,7 @@ function Locations() {
             </Marker>
           </MapContainer>
         </div>
-        <div className='py-20 px-6 text-center bg-bg w-full bg-locationoval bg-no-repeat bg-left-top'>
+        <div className='py-20 px-6 text-center bg-bg w-full bg-locationoval bg-no-repeat bg-left-top md:rounded-2xl'>
           <h1 className='px32peach mb-6'>Canada</h1>
           <p className='px15grey font-bold'>Designo Central Office</p>
           <p className='px15grey font-normal'>3886 Wellington Street</p>
@@ -29,8 +29,8 @@ function Locations() {
           <p className='px15grey font-normal'>M : contact@designo.co</p>
         </div>
       </div>
-      <div className='flex flex-col items-center h-screen mb-10'>
-        <div className='h-80 w-full'>
+      <div className='flex flex-col items-center h-screen md:mx-10 md:mb-32'>
+        <div className='h-80 w-full mb-8 md:rounded-2xl overflow-hidden'>
           <MapContainer center={["-30.329530", "149.788190"]} zoom={13} style={{ height: '100%', width: '100%', margin: 0, padding: 0 }} zoomControl={false}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -43,7 +43,7 @@ function Locations() {
             </Marker>
           </MapContainer>
         </div>
-        <div className='py-20 px-6 text-center bg-bg w-full bg-locationoval bg-no-repeat bg-left-top'>
+        <div className='py-20 px-6 text-center bg-bg w-full bg-locationoval bg-no-repeat bg-left-top md:rounded-2xl'>
           <h1 className='px32peach mb-6'>Australia</h1>
           <p className='px15grey font-bold'>Designo AU Office</p>
           <p className='px15grey font-normal'>19 Balonne Street</p>
@@ -53,8 +53,8 @@ function Locations() {
           <p className='px15grey font-normal'>M : contact@designo.au</p>
         </div>
       </div>
-      <div className='flex flex-col items-center h-screen mb-10'>
-        <div className='h-80 w-full'>
+      <div className='flex flex-col items-center h-screen mb-10 md:mx-10'>
+        <div className='h-80 w-full md:rounded-2xl md:mb-8 overflow-hidden'>
           <MapContainer center={["51.141530", "1.297640"]} zoom={13} style={{ height: '100%', width: '100%', margin: 0, padding: 0 }} zoomControl={false}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -67,7 +67,7 @@ function Locations() {
             </Marker>
           </MapContainer>
         </div>
-        <div className='py-20 px-6 text-center bg-bg w-full bg-locationoval bg-no-repeat bg-left-top'>
+        <div className='py-20 px-6 text-center bg-bg w-full bg-locationoval bg-no-repeat bg-left-top md:rounded-2xl'>
           <h1 className='px32peach mb-6'>United Kingdom</h1>
           <p className='px15grey font-bold'>Designo UK Office</p>
           <p className='px15grey font-normal'>13  Colorado Way</p>
